8d7d9fb3faf223d872347c5a0741f6b7dca0780a
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-05 00:57:49 +0300

    Completed the task(no UI for now )

---
+++ script.js
@@ -0,0 +1,42 @@
+function getComputerChoice(){
+  //Generate random number from 0 to 2 
+  const randomNumber = Math.floor(Math.random() *3);
+  //Assigning return depending on what number we got
+  switch(randomNumber){
+    case 0 : 
+      return "rock";
+    case 1 :
+      return "paper";
+    case 2 : 
+      return "scissors";
+  }
+}
+
+function  playRound(playerSelection, computerSelection){
+  playerSelection = prompt("Rock, Paper or Scissors ?")
+  computerSelection = getComputerChoice();
+  //Makes player input case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+  //Checks if playerSelection was rock, paper or scissors
+  if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
+    return "Invalid choice! Please choose Rock, Paper, or Scissors"
+  } 
+  
+  if (playerSelection === computerSelection){
+    return "It`s a tie"
+  } else if (playerSelection === "rock"  && computerSelection === "scissors" ||
+    playerSelection === "scissors" && computerSelection === "paper" ||
+    playerSelection === "paper" && computerSelection === "rock"){
+    return "You win! Rock beats Scissors";
+  } else {
+    return `You loose ${computerSelection} beats ${playerSelection}`
+  }    
+}
+
+function playgame(){
+  console.log(playRound());
+  console.log(playRound());
+  console.log(playRound());
+  console.log(playRound());
+  console.log(playRound());
+}
