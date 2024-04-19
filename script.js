4d313c207b91fe77995262612f390570d54174d1
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-19 19:18:56 +0300

    complete ui and functional though there is a trouble with display choice being standalone and abscense of pop-up

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+
+let gameResult = document.querySelector("#game-result");
+let gameInfo = document.querySelector("#game-message");
+
+let rockBtn = document.querySelector("#rockbtn");
+let paperBtn = document.querySelector("#paperbtn");
+let scissorsBtn = document.querySelector("#scissorsbtn");
+
+let playerChoice = document.querySelector("#player-choice");
+let computerChoice = document.querySelector("#computer-choice")
+
+let playerScore = 0;
+let computerScore = 0;
+
+let playerScorePara = document.querySelector("#player-score");
+let computerScorePara = document.querySelector("#computer-score");
+
+
+
+rockBtn.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playRound("rock", computerSelection);
+  displayChoice(playerChoice, "rock");
+  displayChoice(computerChoice, computerSelection);
+});
+
+paperBtn.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playRound("paper", computerSelection);
+  displayChoice(playerChoice, "paper");
+  displayChoice(computerChoice, computerSelection);
+});
+
+scissorsBtn.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playRound("scissors", computerSelection);
+  displayChoice(playerChoice, "scissors");
+  displayChoice(computerChoice, computerSelection);
+});
+
+
+
+function displayChoice(element, choice){
+  let sign = "";
+  switch (choice) {
+    case "rock":
+      sign = "✊";
+      break;
+    case "paper":
+      sign = "✋";
+      break;
+    case "scissors":
+      sign = "✌";
+      break;
+  }
+  element.textContent = sign;
+  element.className = "sign";
+}
+
+
 function getComputerChoice(){
   //Generate random number from 0 to 2 
   const randomNumber = Math.floor(Math.random() *3);
@@ -12,22 +72,50 @@ function getComputerChoice(){
   }
 }
 
+
 function  playRound(playerSelection, computerSelection){
   //Makes player input case-insensitive
-  playerSelection = playerSelection.toLowerCase();
+  //playerSelection = playerSelection.toLowerCase();
   //Checks if playerSelection was rock, paper or scissors and not something else
-  if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
-    return "Invalid choice! Please choose Rock, Paper, or Scissors"
-  } 
+  // if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
+  //   return "Invalid choice! Please choose Rock, Paper, or Scissors"
+  // } 
   
   if (playerSelection === computerSelection){
-    return "It`s a tie"
+
+    gameResult.textContent = "It`s a tie";
+    gameInfo.textContent = `${playerSelection} ties with ${computerSelection}`;
+
   } else if (playerSelection === "rock"  && computerSelection === "scissors" ||
     playerSelection === "scissors" && computerSelection === "paper" ||
     playerSelection === "paper" && computerSelection === "rock"){
-    return "You win! Rock beats Scissors";
+
+    gameResult.textContent = "You win!";
+    gameInfo.textContent = `${playerSelection} beats ${computerSelection}`;
+    playerScore++
+
   } else {
-    return `You loose ${computerSelection} beats ${playerSelection}`
+    gameResult.textContent = "You loose"
+    gameInfo.textContent = `${computerSelection} beats ${playerSelection}`;
+    computerScore++
   }    
+  playerScorePara.textContent = `Player score: ${playerScore}`;
+  computerScorePara.textContent = `Computer score: ${computerScore}`;
+
+
+  if (playerScore == 5 || computerScore == 5){
+    if (playerScore > computerScore) {
+      alert("You Won!");
+    } else if (playerScore < computerScore) {
+      alert("You Lost :(");
+    } 
+    
+    playerScore = 0;
+    computerScore = 0;
+    playerScorePara.textContent = "Player score: 0";
+    computerScorePara.textContent = "Computer score: 0";
+    gameResult.textContent = "Choose Rock, Paper, or Scissors";
+    gameInfo.textContent = "First to score 5 points wins the game";
+  }
 }
 
